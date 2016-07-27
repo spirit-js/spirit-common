eb248099f46f30c2fb8111778d2c69d9e3a3528f
hc <henycr@gmail.com>
2016-07-27 15:34:30 -0400

    add body parser middleware

--- spec/body-parser-spec.js
+++ spec/body-parser-spec.js
@@ -5,29 +5,75 @@ const http = require("http")
 const request = require("superagent")
 
 describe("body-parser", () => {
-  let server
+  var server
+  var result
 
   beforeAll(() => {
     const handler = (request) => {
-      return request.body
+      result = request.body
+      return { status: 200, headers: {} }
     }
 
-    server = http.createServer(spirit.node.adapter(handler, [parser]))
+    server = http.createServer(spirit.node.adapter(handler, [parser()]))
     server.listen(3009)
+  })
+
+  beforeEach(() => {
+    result = undefined
   })
 
   afterAll(() => {
     server.close()
   })
 
-  it("ok", (done) => {
+  it("json ok", (done) => {
     request.get("http://localhost:3009")
+      .send({ a: 1, b: 2 })
       .end((err, res) => {
-        expect(res.status).toBe(200)
         expect(res.text).toBe("")
+        expect(res.status).toBe(200)
+        expect(result).toEqual({
+          a: 1,
+          b: 2
+        })
         done()
       })
-    
+  })
+
+  it("form ok", (done) => {
+    request.get("http://localhost:3009")
+      .send("name=hi")
+      .send("id=1")
+      .end((err, res) => {
+        expect(res.status).toBe(200)
+        expect(result).toEqual({
+          name: "hi",
+          id: "1"
+        })
+        done()
+      })
+  })
+
+  it("text ok", (done) => {
+    request.get("http://localhost:3009")
+      .set("Content-Type", "text/plain")
+      .send("hello how are you")
+      .end((err, res) => {
+        expect(res.status).toBe(200)
+        expect(result).toBe("hello how are you")
+        done()
+      })
+  })
+
+  it("unknown content-type is ignored, should be undefined request body", (done) => {
+    request.get("http://localhost:3009")
+      .set("Content-Type", "a")
+      .send("hello how are you")
+      .end((err, res) => {
+        expect(res.status).toBe(200)
+        expect(result).toBe(undefined)
+        done()
+      })
   })
 
 })
--- src/body-parser.js
+++ src/body-parser.js
@@ -1,17 +1,79 @@
-const parser = require("co-body")
+// TODO
+// implementation relies on raw-body thus global Promise to exist
 
-module.exports = (handler) => {
-  return (request) => {
-    return parser(request.req())
-      .then((body) => {
-        request.body = body
-        return handler(request)
+const typeis = require("type-is")
+const inflate = require("inflation")
+const parser = require("raw-body")
+const qs = require("querystring")
+
+const types = {
+  json: {
+    type_string: ["json", "application/*+json", "application/csp-report"],
+    run: (req, opts) => {
+      opts.limit = opts.limit || "1mb"
+      return parser(req, opts).then((result) => {
+        return JSON.parse(result)
       })
-      .catch((err) => {
-        // this could be an actual err,
-        // or a unknown content-type err (not important)
-        // but just surpress all
-        return handler(request)
+    }
+  },
+
+  form: {
+    type_string: ["urlencoded"],
+    run: (req, opts) => {
+      opts.limit = opts.limit || "56kb"
+      return parser(req, opts).then((result) => {
+        return qs.parse(result)
       })
+    }
+  },
+
+  text: {
+    type_string: ["text"],
+    run: (req, opts) => {
+      opts.limit = opts.limit || "1mb"
+      return parser(req, opts)
+    }
+  }
+}
+
+const default_opts = (opts, req) => {
+  const len = req.headers["content-length"]
+  const encoding = req.headers["content-encoding"] || "identity"
+  if (len && encoding === "identity") opts.length = ~~len
+  opts.encoding = opts.encoding || "utf8"
+  return opts
+}
+
+module.exports = (opts) => {
+  const check = Object.keys(types)
+  if (opts === undefined) opts = {}
+  check.forEach((t) => {
+    if (opts[t] === undefined) opts[t] = {}
+  })
+
+  return (handler) => {
+    return (request) => {
+      const req = request.req()
+      const ctype = request.headers["content-type"]
+      // typeis() already does these checks, but do it here
+      // for quicker exit
+      if (typeis.hasBody(req) === false || ctype === undefined) {
+        return handler(request)
+      }
+
+      for (var i = 0; i < check.length; i++) {
+        var t = check[i]
+        if (opts[t] !== false
+            && typeis.is(ctype, types[t].type_string)) {
+          return types[t].run(inflate(req), default_opts(opts[t], req))
+            .then((body) => {
+              request.body = body
+              return handler(request)
+            })
+        }
+      }
+
+      return handler(request)
+    }
   }
 }
